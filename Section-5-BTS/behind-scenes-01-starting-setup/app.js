d0ffd84c8309f3a8ca7fc3b4ee10359b77e87acc
Banskiie <ivansinohon28@gmail.com>
2023-03-13 09:16:45 +0800

    new

---
+++ Section-5-BTS/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,15 @@
+var name = 'Ivan';
+
+if (name === 'Ivan') {
+    var hobbies = ['Sports', 'Coding'];
+    console.log(hobbies)
+}
+
+function greet() {
+    var age = 25;
+    // let name = 'Max';
+    console.log(name, age);
+}
+
+console.log(name, hobbies);
+greet();
