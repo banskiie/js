34ad5444ab68b9e0ca7f4bbea99411ea33f22eea
banskiie <ivansinohon28@gmail.com>
2023-03-18 13:53:32 +0800

    test

--- Section-8-Arrays/arrays-assignment-problem/assignment.js
+++ Section-8-Arrays/arrays-assignment-problem/assignment.js
@@ -1,22 +1,53 @@
-const numArr = [2, 4, 6, 8, 10];
-const filterArr = numArr.filter(val => val > 6);
-console.log(filterArr);
+const numArr = [1, 3, 5, 7, 9, 11];
+
+// const filArr = numArr.filter((val, idx, arr) => {
+//     return val > 6;
+// });
+const filArr = numArr.filter(val => val > 6);
+console.log(filArr);
+
+// const mapArr = numArr.map((val, idx, arr) => {
+//     const newObj = { num: val };
+//     return newObj;
+// });
 const mapArr = numArr.map(val => ({ num: val }));
 console.log(mapArr);
-const reducedVal = numArr.reduce((prev, cur) => prev * cur, 1);
-console.log(reducedVal);
 
-const findMax = (...numbers) => Math.max(...numbers);
+// const redVal = numArr.reduce((prev, cur, idx, arr) => { return prev *= cur }, 1);
+const redVal = numArr.reduce((prev, cur) => prev * cur, 1);
+console.log(redVal);
 
-const findMinMax = (...numbers) => [Math.min(...numbers), Math.max(...numbers)];
-const [minVal, maxVal] = findMinMax(...numArr);
-
+const findMax = (...arr) => {
+  return Math.max(...arr);
+}
 console.log(findMax(...numArr));
-console.log(minVal, maxVal);
 
-const fullName = new Set();
-fullName.add('Shand');
-fullName.add('Ivan');
-fullName.add('Ivan')
-console.log(fullName);
+const findMinMax = (...arr) => {
+  return [Math.min(...arr), Math.max(...arr)];
+}
 
+const [minValue, maxValue] = findMinMax(...numArr);
+console.log(minValue, maxValue);
+
+const slicedArr = numArr.slice(1, 4);
+console.log(slicedArr);
+
+const splicedArr = numArr.splice(6, 0, 13, 15, 17);
+console.log(numArr);
+
+console.log(numArr.includes(14));
+console.log(numArr.find(val => val === 5));
+console.log(numArr.findIndex(val => val === 5));
+console.log(numArr.reverse())
+console.log(numArr.join(", "))
+
+const numSet = new Set();
+numSet.add(1);
+console.log(numSet);
+
+const numMap = new Map();
+
+for (const num of numArr) {
+  numMap.set(`Value #${num}`, num);
+}
+console.log(numMap);
