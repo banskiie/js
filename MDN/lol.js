41f7da52efd5545e6c2dd265d645e2376ff58bc2
Banskiie <ivansinohon28@gmail.com>
2023-03-21 09:16:59 +0800

    start oop

---
+++ MDN/lol.js
@@ -0,0 +1,31 @@
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({ max, min }) => (max + min) / 2.0;
+console.log(half(stats));
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  arr.forEach((val) => { failureItems.push(`<li class="text-warning">${val}</li>`) });
+
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
+console.log(failuresList);
+
--- Section-9-Objects/objects-01-starting-project/objects-01-starting-project/assets/scripts/test.js
+++ Section-9-Objects/objects-01-starting-project/objects-01-starting-project/assets/scripts/test.js
@@ -1,14 +1,14 @@
 const variable = 'gamer';
 
 const person = {
-    name: "Ivan",
-    age: 25,
-    hobbies: ['Eating', 'Coding'],
-    greet: () => {
-        console.log('HELLO!');
-    },
-    1.25: 'nice',
-    [variable]: 'noob'
+  name: "Ivan",
+  age: 25,
+  hobbies: ['Eating', 'Coding'],
+  greet: () => {
+    console.log('HELLO!');
+  },
+  1.25: 'nice',
+  [variable]: 'noob'
 };
 person.greet();
 console.log(person.name);
