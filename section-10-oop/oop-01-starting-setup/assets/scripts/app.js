0b7732e25d68051bc3f9c91a9c1e479600aad2e1
Banskiie <ivansinohon28@gmail.com>
2023-03-22 17:25:00 +0800

    section 10 oop

---
+++ section-10-oop/oop-01-starting-setup/assets/scripts/app.js
@@ -0,0 +1,125 @@
+class Product {
+  // title = 'DEFAULT';
+  // imageUrl;
+  // description;
+  // price;
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
+}
+
+class ShoppingCart {
+  items = [];
+
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1.00}</h2>`
+  }
+
+  render() {
+    const cartEl = document.createElement('section');
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now!</button>
+    `;
+    cartEl.className = 'cart';
+    this.totalOutput = cartEl.querySelector('h2');
+    return cartEl;
+  }
+}
+
+class ProductItem {
+
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    App.addProductToCart(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+      <div>
+        <img src="${this.product.imageUrl}" alt="${this.product.title}">
+      <div class="product-item__content">
+        <h2>${this.product.title}</h2>
+        <h3>\$${this.product.price}</h3>
+        <p>${this.product.description}</p>
+        <button>Add to Cart</button>
+      </div>
+      </div>
+    `;
+    const addCartBtn = prodEl.querySelector('button');
+    addCartBtn.addEventListener('click', this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product
+      (
+        'A Pillow',
+        'http://cdn.shopify.com/s/files/1/0290/6177/5439/products/BoutiqueHotelCollectionBedPillowCase_38335835_800x.jpg?v=1647241323',
+        'A soft pillow',
+        19.99
+      )
+    ,
+    new Product(
+      'A Carpet',
+      'https://upload.wikimedia.org/wikipedia/commons/e/e7/Wollteppich_1.jpg',
+      'A big carpet',
+      69.99
+    )
+  ];
+
+  render() {
+    const prodList = document.createElement('ul');
+    prodList.className = 'product-list';
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
+      prodList.append(prodEl);
+    }
+    return prodList;
+  }
+}
+
+class Shop {
+
+
+  render() {
+    const renderHook = document.getElementById('app');
+
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const productListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(productListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductToCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
+
